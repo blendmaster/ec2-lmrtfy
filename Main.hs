28bc97d9d3be710b338866d5b2ae707e0a061155
Steven Ruppert <blendmaster@gmail.com>
2015-10-10 21:59:46 -0600

    Parse A records from ec2-style hostnames
    For situations where DNS is somehow misconfigured
    or in stupid programs that give you internal `ip-x-x-x-x` hostnames
    (EMR YARN app manager) instead of the damn ip addresses themselves.

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@
 import Prelude hiding (takeWhile)
 import Control.Concurrent (forkIO)
 import Control.Monad (forever)
+import Control.Applicative ( (<|>) )
 import Data.Default (Default(def))
 import Data.IP (IPv4, toIPv4)
 import Data.List (partition)
@@ -12,7 +13,7 @@ import System.Timeout (timeout)
 import Network.Socket.ByteString (sendAll, sendAllTo, recvFrom)
 import Network.Socket hiding (recvFrom)
 import Network.DNS
-import Data.Attoparsec.ByteString.Char8 
+import Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as BL
 
@@ -58,25 +59,37 @@ proxyRequest Conf{..} server req = do
                         else Left "identifier not match"
 
 {--
+ - Parse out EC2-style "hostnames" derived from the IP address, e.g.
+ - ec2-203-0-113-25.compute-1.amazonaws.com. -> 203.0.113.25 
+ -}
+resolveEc2Hostname :: DNSMessage -> Maybe DNSMessage
+resolveEc2Hostname req@(DNSMessage {question = (q@(Question domain A):_)}) =
+    maybeResult $ parse parseEc2 domain
+  where
+    ident = identifier . header $ req
+    parseEc2 :: Parser DNSMessage
+    parseEc2 = do
+        _ <- "ec2-" <|> "ip-"
+        octets <- map read <$> (many1 digit `sepBy` string "-")
+        case length octets of
+          4 -> 
+              return $ responseA ident q [ip]
+            where ip = toIPv4 octets
+          _ -> fail "not an ec2 ip"
+
+resolveEc2Hostname _ = Nothing
+
+{--
  - Handle A query for domain suffixes configured, and proxy other requests to real dns server.
  -}
 handleRequest :: Conf -> DNSFormat -> IO (Either String DNSFormat)
 handleRequest conf req =
-    case lookupHosts of
+    case resolveEc2Hostname req of
         (Just rsp) -> return $ Right rsp
         Nothing  -> maybe
                     (return $ Left "nameserver not configured.")
                     (\srv -> proxyRequest conf srv req)
                     (listToMaybe (nameservers conf))
-  where
-    filterA = filter ((==A) . qtype)
-    filterHost dom = filter (\(h, _) -> h `B.isSuffixOf` dom)
-    ident = identifier . header $ req
-    lookupHosts :: Maybe DNSFormat
-    lookupHosts = do
-        q <- listToMaybe . filterA . question $ req
-        (_, ip) <- listToMaybe . filterHost (qname q) $ hosts conf
-        return $ responseA ident q [ip] 
 
 {--
  - Parse request and compose response.
@@ -86,6 +99,7 @@ handlePacket conf@Conf{..} sock addr s =
     either
     (putStrLn . ("decode fail:"++))
     (\req -> do
+        -- putStrLn $ show req
         handleRequest conf req >>=
             either
             putStrLn
